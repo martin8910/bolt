f471b7d6b2d05d80bdfa658f1abaf13793cddee8
Martin [Tendril] <martin@tendril.ca>
2018-06-07 18:20:33 -0400

    - First release of bolt with adding of the launcher from qtCore and similar

---
+++ launcher/main.py
@@ -0,0 +1,39 @@
+from Alfred.core.external.Qt import QtWidgets, QtCore, QtGui
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin, MayaQDockWidget
+
+import os
+from bolt.core import qtCore
+from bolt.core import mayaCore
+
+relativePath = os.path.dirname(os.path.realpath(__file__)) + os.sep
+uiPath = os.path.dirname(os.path.realpath(__file__)) + os.sep + "ui" + os.sep
+
+
+def show():
+    '''Start an instance of the Module info UI'''
+    # Launch an instance of the window
+    global aModuleInfo
+    boldLauncherWindow = bolt_launcher(parent=mayaCore.get_maya_Window())
+    # Show the window
+    boldLauncherWindow.show(dockable=True, floating=True)
+
+class bolt_launcher(MayaQWidgetDockableMixin, QtWidgets.QDialog):
+    '''Instance a module-menu that loads all of the modules from the Alfred.Modules folder'''
+    def __init__(self, parent=None):
+        super(bolt_launcher, self).__init__(parent)
+        try: boldLauncherWindow.close()
+        except: pass
+
+        # UI loading
+        print "{}boltMainUI.ui".format(uiPath)
+        self.ui = qtCore.qtUiLoader("{}boltMainUI.ui".format(uiPath))
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.ui)
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self.layout)
+
+        self.resize(620, 350)
+        # Create window attributes
+        #self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        #self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.Tool)
